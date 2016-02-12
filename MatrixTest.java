b186a1471007159b2164d1e2758fcbe6dd7de1fa
frillgriller <13bscsdkhalid@seecs.edu.pk>
2016-02-12 16:48:48 +0500

    MatrixTest.java

---
+++ MatrixTest.java
@@ -0,0 +1,45 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MatrixTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testMultiply_Old() {
+		int[][] expected = {{16,19},{36,43}};
+		
+		int[][] aTemp = {{1,2},{3,4}};
+		Matrix A = new Matrix(aTemp);
+		
+		int[][] bTemp = {{4,5},{6,7}};
+		Matrix B = new Matrix(bTemp);
+		
+		Assert.assertArrayEquals(expected,A.iterative(B).actual);
+	}
+
+	@Test
+	public final void testMultiply_Strassen() {
+int[][] expected = {{16,19},{36,43}};
+		
+		int[][] aTemp = {{1,2},{3,4}};
+		Matrix A = new Matrix(aTemp);
+		
+		int[][] bTemp = {{4,5},{6,7}};
+		Matrix B = new Matrix(bTemp);
+		
+		Matrix C = new Matrix(2,2);
+		Assert.assertArrayEquals(expected,C.Strassen_Recursive(A.actual,B.actual).actual);
+	}
+
+}
